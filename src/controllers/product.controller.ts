d551d38e2adf113571e6cfc2706dae7a795eaa14
sh1nn0on3 <sanogaku31@gmail.com>
2023-11-11 15:15:17 +0700

    10 done

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -51,7 +51,7 @@ const getProducts = asyncHandler(async (req: Request, res: Response | any) => {
   }
   //Pagination
   const page = Number(req.query.page) || 1
-  const limit = Number(req.query.limit) || 4
+  const limit = Number(req.query.limit) || 10
   const skip = (page - 1) * limit
   products = products.skip(skip).limit(limit)
   //Execute query
